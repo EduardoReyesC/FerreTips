30a38c301aa11354059eb66e78ddf756f1ef05a2
alejandracruz987 <2410527@cesba-queretaro.edu.mx>
2025-05-08 21:14:12 -0600

    nombres de prod.

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, url_for, session
 
 app = Flask(__name__)
 
-
 @app.route('/herramientas')
 def herramientas():
     return render_template('herramientas.html')
